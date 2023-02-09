8e081fc4b4c558ef787d79f56ee9410285927d3e
DurandoAngiulo <87902832+DurandoAngiulo@users.noreply.github.com>
2023-02-09 11:30:42 -0500

    uploaded custom view controller

---
+++ idm362-dpa35/ViewControllerCounter.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerCounter.swift
+//  idm362-dpa35
+//
+//  Created by Durando Angiulo on 2/9/23.
+//
+
+import UIKit
+
+class ViewControllerCounter: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
